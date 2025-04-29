b372237ced506b7f34010504f6cc37925e5e9096
Krishan Gopal <kgmishra21@gmail.com>
2025-04-29 15:22:11 +0530

    added cors

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const path = require("path");
 const blogRoutes = require("./routes/blogRoutes");
 const app = express();
 const useRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Serve static files from uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
